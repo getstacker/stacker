df41f3f2f06ac45abc59aa5e2ae117e9791532e3
Joe Johnston <joe@simple10.com>
2015-03-01 21:06:44 -0800

    Add stacker dir to NODE_PATH

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+var path = require('path');
+if (process.env.NODE_PATH) {
+  process.env.NODE_PATH = __dirname + path.delimiter + process.env.NODE_PATH;
+} else {
+  process.env.NODE_PATH = __dirname;
+}
+
 require('coffee-script/register');
 require('stacker-globals');
 require('./lib/runner').run();
